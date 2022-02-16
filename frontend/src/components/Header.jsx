d7774db19cb515d3998601b4903c5517c16279f5
tessoka <akos.gollitz@gmail.com>
2022-02-16 19:23:38 +0100

    searchbar up and running

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { SearchLogo, RocketLogo } from '../svg/svg-d-texts'
 
 
 const Header = () => {
+
+  const [ isSearchOpen, setIsSearchOpen] = useState(false)
+  const [ searchText, setSearchText ] = useState("")
+  
+  const handleClickSearchIcon = () => {
+    setSearchText("")
+    setIsSearchOpen(!isSearchOpen)
+  }
+
+  console.log(searchText)
+
+
   return (
     <header>
       <div className="logo-box">
@@ -13,14 +25,25 @@ const Header = () => {
       </div>
       <div className="header-mid">
         <Link to="/cocktail">
-          <button className="btn">Get a Cocktail</button>
+          <button className="btn">
+            {searchText === "" ?
+            "Get a Cocktail"
+            :
+            "Search"
+            }
+          </button>
         </Link>
       </div>
       <div className="search-box">
-        <div className="icon-bg-search">
+        <div className="icon-bg-search" onClick={handleClickSearchIcon}>
           <svg className="search-logo" viewBox="0 0 512 512"><path d={SearchLogo} /></svg>
         </div>
       </div>
+     
+      {
+      isSearchOpen &&
+      <input className="search-input" type="text" placeholder="Looking for a cocktail?" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+      }
     </header>
   )
 }
