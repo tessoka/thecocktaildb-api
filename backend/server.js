7d340e70751e4198e9fc49dc005ddfa8444ced40
tessoka <akos.gollitz@gmail.com>
2022-02-16 15:10:36 +0100

    back btn to home component done

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,27 @@ const app = express()
 const cors = require("cors")
 
 
-// MIDDLEWARES
-app.use(cors())
-app.use(express.json())
-
+// CHECK if server
 app.use("/", (req, res, next) => {
   console.log("up and running")
   next()
 })
+
+
+// IMPORT ROUTES
+const cocktailRoute = require("./routes/cocktail")
+
+// MIDDLEWARES
+app.use(cors())
+app.use(express.json())
+
+
+// ROUTE MIDDLEWARES
+app.use("/api/cocktail", cocktailRoute)
+
+
+
+
 
 
 app.listen(5000)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Home from './components/Home'
 import Cocktail from './components/Cocktail'
+import Back from './components/Back'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
             <Route path="/cocktail" element={<Cocktail />} />
           </Routes>
         </main>
+        <Back />
       </div>
     </Router>
   )
