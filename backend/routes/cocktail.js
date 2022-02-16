7f52371d644e3a24c39cb2d15b4f4fbf218fc3a9
tessoka <akos.gollitz@gmail.com>
2022-02-16 22:11:50 +0100

    added some consol logs

--- backend/routes/cocktail.js
+++ backend/routes/cocktail.js
@@ -3,15 +3,18 @@ const router = express.Router()
 const axios = require('axios')
 
 router.get("/", async (req, res, next) => {
+// SET URL
   let apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-  if (req.query.s !== "") apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.query.s}`
-  console.log(req.query)
+  if (req.query.s !== "") {
+    apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.query.s}`
+    console.log("Searched string: " + req.query.s)
+  }
 
+// FETCH API
   try {
     const response = await axios.get(apiUrl)
     res.status(200).json(response.data)
   } catch (error) {
-    console.log(error)
     res.json(error)
   }
 })
