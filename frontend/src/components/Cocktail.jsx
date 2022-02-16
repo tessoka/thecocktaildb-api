654a80d288065819a3a51db529fa504aef8d3f36
tessoka <akos.gollitz@gmail.com>
2022-02-16 14:29:52 +0100

    basic fatch setup on Cocktail.jsx

--- frontend/src/components/Cocktail.jsx
+++ frontend/src/components/Cocktail.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { backendUrl } from '../utilities/ServerUrl'
+
 
 const Cocktail = () => {
+
+  const [ isLoading, setIsLoading ] = useState(true)
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(backendUrl + "/api/cocktail")
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    console.log("useEffect was triggered")
+    getData()
+  }, [])
+
   return (
     <div>Cocktail</div>
   )
